5553a66737edf153f5f03b5738632a705ac06bca
EnriWildBandana <EnriWildBandana@users.noreply.github.com>
2019-05-24 22:33:41 +0200

    Update Preda.java

--- src/Preda.java
+++ src/Preda.java
@@ -8,25 +8,29 @@ public class Preda {
     public int breed_time;
     private boolean has_moved;
     private int tempo_di_riproduzione;
+    private coordinates coor;
     private Random rand;
 
-    public Preda(int breed_time){
+    public Preda(int breed_time, coordinates coor){
         this.breed_time = breed_time;
         this.has_moved = false;
+        this.coor = coor;
         this.rand = new Random();
     }
 
     public coordinates move(ArrayList<coordinates> free_cell, coordinates actual_coordinates){
-    
-        coordinates six = new coordinates(actual_coordinates.x + 1, actual_coordinates.y);
-        coordinates seven = new coordinates(actual_coordinates.x - 1, actual_coordinates.y + 1);
-        coordinates eight = new coordinates(actual_coordinates.x, actual_coordinates.y + 1);
-        coordinates nine = new coordinates(actual_coordinates.x + 1, actual_coordinates.y + 1);
 
-        if (this.has_moved || free_cell.size() == 0) return new coordinates(-1, -1);
-        coordinates new_pos = free_cell.remove( rand.nextInt(free_cell.size()));
+        coordinates six = new coordinates(this.coor.x + 1, this.coor.y);
+        coordinates seven = new coordinates(this.coor.x - 1, this.coor.y + 1);
+        coordinates eight = new coordinates(this.coor.x, this.coor.y + 1);
+        coordinates nine = new coordinates(this.coor.x + 1, this.coor.y + 1);
+
+        if (this.has_moved || free_cell.size() == 0) return new coordinates(-1, -1); // se mi sono già mosso o non ho spazio intorno
+        coordinates new_pos = free_cell.remove( rand.nextInt(free_cell.size())); // altrimenti genero una coordinata casuale
         this.tempo_di_riproduzione += 1;
-        if (new_pos == six || new_pos == seven || new_pos == eight || new_pos == nine) this.has_moved = true;
+        if (new_pos.compares(six)|| new_pos.compares(seven) || new_pos.compares(eight) || new_pos.compares(nine)) this.has_moved = true;
+        this.coor.x = new_pos.x;
+        this.coor.y = new_pos.y;
         return new_pos;
     }
 
