595e2859bd867662e2fb2b800c305877b230c1c7
Hangovers <betanymph@live.com>
2019-05-25 12:00:26 +0200

    Aggiunti commenti e corretto il metodo dead. Accorciato il metodo move.

--- src/Predatore.java
+++ src/Predatore.java
@@ -6,57 +6,71 @@ public class Predatore {
     public int energy;
     public int breed_energy;
     private int has_moved;
-    private Random rand;
+    private Random r = new Random();
 
     public Predatore(int energy, int breed_energy) {
         //Crea un nuovo predatore
+
         this.energy = energy;
         this.breed_energy = breed_energy;
         this.has_moved = 0;
-        this.rand = new Random();
+
     }
+
 
     public coordinates move(fish_free_pair tuple, coordinates actual_coordinates) {
         //Muove il predatore in uno spazio adiacente a caso, dando priorita` agli spazi con delle prede da mangiare.
 
+        if (this.has_moved == 1 || (fish.size() == 0 && free_cell.size() == 0)) return new coordinates(-1, -1);
+
+        //Coordinate nella quali il parametro has_moved deve venire settato a 1.
         coordinates six = new coordinates(actual_coordinates.x + 1, actual_coordinates.y);
         coordinates seven = new coordinates(actual_coordinates.x - 1, actual_coordinates.y + 1);
         coordinates eight = new coordinates(actual_coordinates.x, actual_coordinates.y + 1);
         coordinates nine = new coordinates(actual_coordinates.x + 1, actual_coordinates.y + 1);
 
-        ArrayList<coordinates> fish = tuple.fish;
-        ArrayList<coordinates> free_cell = tuple.free_cell;
-
-        if (this.has_moved == 1 || (fish.size() == 0 && free_cell.size() == 0)) return  new coordinates(-1,-1);
-
-        if (fish.size() != 0){
-            int where_to_move = rand.nextInt(fish.size());
-            coordinates new_pos = fish.get(where_to_move);
-            if ( new_pos == six || new_pos == seven || new_pos == eight || new_pos == nine) this.has_moved = 1;
-            return new_pos;
-        }
+        /***************************************************************************************************************
+        * i nomi delle coordinates definita qui sopra sono questi perche` la griglia delle mosse e` pensata come segue.*
+        *                                                                                                              *
+        * 1   2   3                                                                                                    *
+        * 4   0   6                                                                                                    *
+        * 7   8   9                                                                                                    *
+        *                                                                                                              *
+        * La posizione 0 e` la posizione attuale. Se si muove nelle caselle 6,7,8 o 9. quando quelle celle verranno    *
+        * controllate, il predatore con has_moved settato a 1 non dovra` muoversi di nuovo.                            *
+        ****************************************************************************************************************/
 
         else {
-            int where_to_move = rand.nextInt(free_cell.size());
-            coordinates new_pos = free_cell.get(where_to_move);
-
-            if (new_pos == six || new_pos == seven || new_pos == eight || new_pos == nine) this.has_moved = 1;
-            return new_pos;
-
+            //Selezione di quale delle due arraylist utilizzare per decidere la mossa successiva.
+            if (tuple.fish.size() != 0) ArrayList<coordinates> moves = tuple.fish;
+            else {
+                ArrayList<coordinates> moves = tuple.free_cell;
+            }
         }
-    }
 
+        //Selezione pseudo-randomica della coordinata dove muoversi ed eventuale settaggio a 1 di has_moved.
+        int where_to_move = r.nextInt(moves.size());
+        coordinates new_pos = moves.get(where_to_move);
+        if ( new_pos == six || new_pos == seven || new_pos == eight || new_pos == nine) this.has_moved = 1;
+        return new_pos;
+        
+    }
+    
 
     public coordinates breed(ArrayList<coordinates> free_cell) {
+        //Ritorna le coordinate dove riprodursi se il predatore puo` riprodursi. Se non puo` ritorna le coordinate -1,-1.
+
         if(free_cell.size() == 0 || this.energy < this.breed_energy) return new coordinates(-1,-1);
-        coordinates new_pos = free_cell.remove( rand.nextInt(free_cell.size()));
+        coordinates new_pos = free_cell.get(r.nextInt(free_cell.size()));
         return new_pos;
     }
 
-    public int dead() {
+
+    public Boolean dead() {
         //ritorna 1 se il predatore ha 0 di energia e deve morire, 0 altrimenti.
-        if (this.energy == 0) return 1;
-        return 0;
+
+        if (this.energy == 0) return true;
+        return false;
     }
 
 }
